f70ab36e0af22e19cdcb83ac418dc3981e9a2f68
Tayfun228 <tccamalbyli@gmail.com>
2020-08-26 21:44:01 +0400

    tayfun

--- home/views.py
+++ home/views.py
@@ -10,5 +10,14 @@ class HomeView(TemplateView):
 class PostProjectView(TemplateView):
     template_name='post-project.html'
 
+
 class GetStartedView(TemplateView):
     template_name='get-started.html'
+
+
+class MyProfileView(TemplateView):
+    template_name='my-profile.html'
+
+
+class MyProfileEditView(TemplateView):
+    template_name='my-profile-edit.html'
