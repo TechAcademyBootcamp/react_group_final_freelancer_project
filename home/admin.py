110fec4886c0c15647cd007a14c32f602b8f2ad5
Atilla45 <atilla.abdulla@gmail.com>
2020-09-21 00:27:03 +0400

    neww

--- home/admin.py
+++ home/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+from home.models import Project,Upload
+
+class ProjectAdmin(admin.ModelAdmin):
+    list_display=('title','author','short_description','long_description','price_type','price_min','price_max','level','admit_time','status','author','currency','upload_files' )
+
+
+
+admin.site.register(Project,ProjectAdmin)
