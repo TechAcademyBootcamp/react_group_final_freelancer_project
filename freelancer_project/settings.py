b7953b7b6a4ffedf99162f8c8589bc86268ab3c0
idris <idris.sabanli@gmail.com>
2020-10-03 21:56:22 +0400

    requirements and docker-compose migrations

--- freelancer_project/settings.py
+++ freelancer_project/settings.py
@@ -160,9 +160,6 @@ WSGI_APPLICATION = 'freelancer_project.wsgi.application'
 
 CHANNEL_LAYERS = {
     'default': {
-        'BACKEND': 'channels_redis.core.RedisChannelLayer',
-        'CONFIG': {
-            "hosts": [('127.0.0.1', 6379)],
-        },
+            "BACKEND": "channels.layers.InMemoryChannelLayer"
     },
 }
